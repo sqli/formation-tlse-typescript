bd868150da8ddc0a95bd60f0d6738c4bc17c54bc
Robin Coma Delperier <rcomadelperier@sqli.com>
2017-07-20 15:20:13 +0200

    practice06

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+var Civility;
+(function (Civility) {
+    Civility["Unknown"] = "(Mr or Ms)";
+    Civility["Mr"] = "Mr";
+    Civility["Ms"] = "Ms";
+})(Civility || (Civility = {}));
+;
 function fullName(civility, lastName, firstName) {
-    if (civility === void 0) { civility = '(Mr or Ms)'; }
+    if (civility === void 0) { civility = Civility.Unknown; }
     var otherFirstNames = [];
     for (var _i = 3; _i < arguments.length; _i++) {
         otherFirstNames[_i - 3] = arguments[_i];
@@ -11,9 +18,9 @@ function fullName(civility, lastName, firstName) {
     return fullName;
 }
 var ex0 = fullName(undefined, 'Coma Delperier');
-var ex1 = fullName('Mr', 'Coma Delperier');
-var ex2 = fullName('Mr', 'Coma Delperier', 'Robin');
-var ex3 = fullName('Mr', 'Coma Delperier', 'Robin', 'Franck', 'Florent');
+var ex1 = fullName(Civility.Mr, 'Coma Delperier');
+var ex2 = fullName(Civility.Mr, 'Coma Delperier', 'Robin');
+var ex3 = fullName(Civility.Mr, 'Coma Delperier', 'Robin', 'Franck', 'Florent');
 console.log(ex0);
 console.log(ex1);
 console.log(ex2);
--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-function fullName(civility: string = '(Mr or Ms)', lastName: string, firstName?: string, ...otherFirstNames: string[]): string{
+enum Civility {
+    Unknown = '(Mr or Ms)',
+    Mr = 'Mr',
+    Ms = 'Ms'
+};
+
+function fullName(civility: Civility = Civility.Unknown, lastName: string, firstName?: string, ...otherFirstNames: string[]): string{
     let fullName = civility + ' ' + lastName;
     if (firstName) {
         fullName += ' ' + firstName + ' ' + otherFirstNames.join(' ');
@@ -7,9 +13,9 @@ function fullName(civility: string = '(Mr or Ms)', lastName: string, firstName?:
 }
 
 const ex0 = fullName(undefined, 'Coma Delperier');
-const ex1 = fullName('Mr', 'Coma Delperier');
-const ex2 = fullName('Mr', 'Coma Delperier', 'Robin');
-const ex3 = fullName('Mr', 'Coma Delperier', 'Robin', 'Franck', 'Florent');
+const ex1 = fullName(Civility.Mr, 'Coma Delperier');
+const ex2 = fullName(Civility.Mr, 'Coma Delperier', 'Robin');
+const ex3 = fullName(Civility.Mr, 'Coma Delperier', 'Robin', 'Franck', 'Florent');
 
 console.log(ex0);
 console.log(ex1);
