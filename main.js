99c6fdcd9fbc69c783a8ccaee4a94c83ca282362
Robin Coma Delperier <rcomadelperier@sqli.com>
2017-07-20 15:02:48 +0200

    practice05

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-var someValue = 'Hello World';
-var strLength1 = someValue.length;
-var strLength2 = someValue.length;
-console.log('strLength1: ', strLength1);
-console.log('strLength2: ', strLength2);
+function fullName(civility, lastName, firstName) {
+    if (civility === void 0) { civility = '(Mr or Ms)'; }
+    var otherFirstNames = [];
+    for (var _i = 3; _i < arguments.length; _i++) {
+        otherFirstNames[_i - 3] = arguments[_i];
+    }
+    var fullName = civility + ' ' + lastName;
+    if (firstName) {
+        fullName += ' ' + firstName + ' ' + otherFirstNames.join(' ');
+    }
+    return fullName;
+}
+var ex0 = fullName(undefined, 'Coma Delperier');
+var ex1 = fullName('Mr', 'Coma Delperier');
+var ex2 = fullName('Mr', 'Coma Delperier', 'Robin');
+var ex3 = fullName('Mr', 'Coma Delperier', 'Robin', 'Franck', 'Florent');
+console.log(ex0);
+console.log(ex1);
+console.log(ex2);
+console.log(ex3);
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-let someValue: any = 'Hello World';
+function fullName(civility: string = '(Mr or Ms)', lastName: string, firstName?: string, ...otherFirstNames: string[]): string{
+    let fullName = civility + ' ' + lastName;
+    if (firstName) {
+        fullName += ' ' + firstName + ' ' + otherFirstNames.join(' ');
+    }
+    return fullName;
+}
 
-const strLength1: number = (<string>someValue).length;
-const strLength2: number = (someValue as string).length;
+const ex0 = fullName(undefined, 'Coma Delperier');
+const ex1 = fullName('Mr', 'Coma Delperier');
+const ex2 = fullName('Mr', 'Coma Delperier', 'Robin');
+const ex3 = fullName('Mr', 'Coma Delperier', 'Robin', 'Franck', 'Florent');
 
-console.log('strLength1: ', strLength1);
-console.log('strLength2: ', strLength2);
+console.log(ex0);
+console.log(ex1);
+console.log(ex2);
+console.log(ex3);
